2eba1cf2473ff588a88466019f14135c59d9627c
Ruslan <ruslanzarichnyi@ukr.net>
2018-04-07 16:01:42 +0300

    API_3

--- src/lesson11/Homework/Controller.java
+++ src/lesson11/Homework/Controller.java
@@ -4,17 +4,18 @@ package lesson11.Homework;
  * Created by Ruslan on 05.04.2018.
  */
 public class Controller {
-    API [] apis;
+    API[] apis;
 
     public Controller(API[] apis) {
         this.apis = apis;
     }
 
-    public Room [] requestRooms (int price, String city, String hotel) {
-
+    public Room[] requestRooms(int price, String city, String hotel) {
+        return new Room[0];
     }
-    public Room check (API api1, API api2) {
 
+    public Room[] check(API api1, API api2) {
+        return new Room[0];
     }
 
 
