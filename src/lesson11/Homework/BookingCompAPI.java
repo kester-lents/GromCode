0229197cd1d821a43d6d8618117d18db8d1bffb1
Ruslan <ruslanzarichnyi@ukr.net>
2018-04-07 15:58:42 +0300

    API_2

--- src/lesson11/Homework/BookingCompAPI.java
+++ src/lesson11/Homework/BookingCompAPI.java
@@ -4,15 +4,32 @@ package lesson11.Homework;
  * Created by Ruslan on 05.04.2018.
  */
 public class BookingCompAPI implements API {
-    Room [] rooms;
+    Room[] rooms;
 
     public BookingCompAPI(Room[] rooms) {
         this.rooms = rooms;
     }
 
+
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
-        return new Room[0];
+        Room[] results = new Room[rooms.length];
+        int index = 0;
+
+        for (Room each : rooms) {
+            if (each.getPrice() == price && each.getPersons() == persons &&
+                    each.getCityName().equals(city) && each.getHotelName().equals(city) && rooms[index] != null) {
+                results[index] = each;
+                index++;
+                if (each.getPrice() - 100 < 0)
+                    if (each.getPrice() >= each.getPrice() - 100 && each.getPrice() <= each.getPrice() + 100)
+                        results[index] = each;
+                index++;
+            }
+        }
+
+
+        return results;
     }
 
     @Override
--- src/lesson11/Homework/GoogleAPI.java
+++ src/lesson11/Homework/GoogleAPI.java
@@ -4,7 +4,7 @@ package lesson11.Homework;
  * Created by Ruslan on 05.04.2018.
  */
 public class GoogleAPI implements API {
-    Room [] rooms;
+    Room[] rooms;
 
     public GoogleAPI(Room[] rooms) {
         this.rooms = rooms;
@@ -12,11 +12,36 @@ public class GoogleAPI implements API {
 
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
-        return new Room[0];
+        int i = 0;
+        getAll();
+
+        for (Room room : rooms) {
+            if (price == room.getPrice() && persons == room.getPersons() && room.getCityName().equals(city)
+                    && room.getHotelName().equals(city) && rooms[i] != null) {
+                getAll()[i] = room;
+            }
+            i++;
+        }
+
+        return getAll();
     }
 
     @Override
     public Room[] getAll() {
-        return new Room[0];
+        int i = 0;
+        int index = -1;
+        for (Room room : rooms) {
+            if (room != null)
+                index++;
+        }
+        Room[] result = new Room[index];
+
+        for (Room room : rooms) {
+            if (rooms[i] != null) {
+                result[i] = room;
+            }
+            i++;
+        }
+        return result;
     }
 }
