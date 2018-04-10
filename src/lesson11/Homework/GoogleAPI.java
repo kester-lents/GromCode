277ea838f3894124cd7c32038fe5af9d252af37f
Ruslan <ruslanzarichnyi@ukr.net>
2018-04-10 12:05:40 +0300

    API_12

--- src/lesson11/Homework/GoogleAPI.java
+++ src/lesson11/Homework/GoogleAPI.java
@@ -1,5 +1,7 @@
 package lesson11.Homework;
 
+import java.util.Arrays;
+
 /**
  * Created by Ruslan on 05.04.2018.
  */
@@ -15,15 +17,23 @@ public class GoogleAPI implements API {
 
     */
 
+    public String toString() {
+        return Arrays.toString(rooms);
+    }
+
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
         int index = 0;
-        Room[] result = new Room[rooms.length];
+        int i = 0;
+        Room[] result = new Room[getAll().length];
         for (Room room : rooms) {
-            if (room.getPrice() == price && room.getPersons() == persons && room.getCityName().equals(city)
-                    && room.getHotelName().equals(hotel) && room != null) {
-                result[index] = room;
-                index++;
+            if (rooms[i] != null) {
+                if (room.getPrice() == price && room.getPersons() == persons && room.getCityName().equals(city)
+                        && room.getHotelName().equals(hotel)) {
+                    result[index] = rooms[i];
+                    index++;
+                }
+                i++;
             }
         }
 
