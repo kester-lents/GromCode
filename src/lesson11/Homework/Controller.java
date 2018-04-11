62f3738bbd8ef1f70e7d5eabe192919980324257
Ruslan <ruslanzarichnyi@ukr.net>
2018-04-11 12:00:53 +0300

    API_31

--- src/lesson11/Homework/Controller.java
+++ src/lesson11/Homework/Controller.java
@@ -50,7 +50,7 @@ public class Controller {
         int length = 0;
         for (Room room2 : api2.getAll()) {
             for (Room room : api1.getAll()) {
-                if (api1.getAll()[i] != null && api2.getAll()[a] != null) {
+                if (api1.getAll()[i] != null && api2.getAll()[a] != null && room2 != null && room != null) {
                     if (api1.getAll()[i].getPersons() == api2.getAll()[a].getPersons() &&
                             api1.getAll()[i].getPrice() == api2.getAll()[a].getPrice() &&
                             api1.getAll()[i].getHotelName().equals(api2.getAll()[a].getHotelName()) &&
@@ -71,7 +71,7 @@ public class Controller {
         int r = 0;
         for (Room room2 : api2.getAll()) {
             for (Room room : api1.getAll()) {
-                if (api1.getAll()[q] != null && api2.getAll()[w] != null) {
+                if (api1.getAll()[q] != null && api2.getAll()[w] != null && room2 != null && room != null) {
                     if (api1.getAll()[q].getPersons() == api2.getAll()[w].getPersons() &&
                             api1.getAll()[q].getPrice() == api2.getAll()[w].getPrice() &&
                             api1.getAll()[q].getHotelName().equals(api2.getAll()[w].getHotelName()) &&
