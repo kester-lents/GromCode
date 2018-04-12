5c9806dece71e76e75702824d4c3cbbd2cada7dc
Ruslan <ruslanzarichnyi@ukr.net>
2018-04-12 16:04:04 +0300

    Big OOP Task5

--- src/Lesson12/UkrainianBankSystem.java
+++ src/Lesson12/UkrainianBankSystem.java
@@ -13,40 +13,11 @@ public class UkrainianBankSystem implements BankSystem {
         //снять деньги
         if (!checkWithdraw(user, amount)) {
             return;
-        }
-         else user.setBalance(user.getBalance() - amount - (amount * user.getBank().getCommission(amount)));
+        } else user.setBalance(user.getBalance() - amount - (amount * user.getBank().getCommission(amount)));
     }
 
-    @Override
-    public void fund(User user, int amount) {
-        if (!checkLimitOfFunding(user, amount, user.getBank().getLimitOfFunding()))
-            return;
-        user.setBalance(user.getBalance() + amount + amount * user.getBank().getCommission(amount));
-    }
-
-    @Override
-    public void transferMoney(User fromUser, User toUser, int amount) {
-        //1. Проверить, есть ли на счете и проходит ли лимит снятия фромюзер
-        //2. Проверить, не превышен ли лимит пополнения у туЮзера.
-        //3. Перевести.
-        if (!checkWithdraw(fromUser, amount))
-            return;
-        if (!checkLimitOfFunding(toUser, amount, toUser.getBank().getLimitOfFunding()))
-            return;
-        fromUser.setBalance(fromUser.getBalance() - amount - amount * fromUser.getBank().getCommission(amount));
-        toUser.setBalance(toUser.getBalance() + amount - amount * toUser.getBank().getCommission(amount));
-        ////////////Check!!!!! - amount&
-    }
-
-    @Override
-    public void paySalary(User user) {
-        if (!checkLimitOfFunding(user, user.getSalary(), user.getBank().getLimitOfFunding()))
-            return;
-        user.setSalary(user.getSalary());
-    }
-
-    private void printWithdrawalErrorMsd(int amount, User user) {
-        System.out.println("Can't withdraw money " + amount + " from user " + user.toString());
+    private void printWithdrawalErrorMsg(int amount, User user) {
+        System.err.println("Can't withdraw money " + amount + " from user " + user.toString());
     }
 
     private boolean checkWithdraw(User user, int amount) {
@@ -55,11 +26,19 @@ public class UkrainianBankSystem implements BankSystem {
     }
 
     private boolean checkWithdrawLimits(User user, int amount, double limit) {
-        if (amount + amount*user.getBank().getCommission(amount) > limit) {
-            printWithdrawalErrorMsd(amount, user);
+        if (amount + amount * user.getBank().getCommission(amount) > limit) {
+            printWithdrawalErrorMsg(amount, user);
             return false;
         }
         return true;
+    }
+
+
+    @Override
+    public void fund(User user, int amount) {
+        if (!checkLimitOfFunding(user, amount, user.getBank().getLimitOfFunding()))
+            return;
+        user.setBalance(user.getBalance() + amount + amount * user.getBank().getCommission(amount));
     }
 
     private boolean checkLimitOfFunding(User user, int amount, double limit) {
@@ -73,5 +52,27 @@ public class UkrainianBankSystem implements BankSystem {
     private void printFundingErrorMsg(User user, int amount) {
         System.err.println("Can't replenish on " + amount + " of user " + user.toString());
     }
+
+    @Override
+    public void transferMoney(User fromUser, User toUser, int amount) {
+        //1. Проверить, есть ли на счете и проходит ли лимит снятия фромюзер
+        //2. Проверить, не превышен ли лимит пополнения у туЮзера.
+        //3. Перевести.
+        if (!checkWithdraw(fromUser, amount))
+            return;
+        if (!checkLimitOfFunding(toUser, amount, toUser.getBank().getLimitOfFunding()))
+            return;
+        fromUser.setBalance(fromUser.getBalance() - amount - amount * fromUser.getBank().getCommission(amount));
+        toUser.setBalance(toUser.getBalance() + amount - amount * toUser.getBank().getCommission(amount));
+    }
+
+    @Override
+    public void paySalary(User user) {
+        if (!checkLimitOfFunding(user, user.getSalary(), user.getBank().getLimitOfFunding()))
+            return;
+        user.setSalary(user.getSalary());
+    }
+
+
 }
 
