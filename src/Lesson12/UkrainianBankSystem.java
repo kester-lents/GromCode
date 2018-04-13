57a9c40f60baeada06681f3c5ae13b978f1d8467
Ruslan <ruslanzarichnyi@ukr.net>
2018-04-13 09:02:16 +0300

    Big OOP Task11

--- src/Lesson12/UkrainianBankSystem.java
+++ src/Lesson12/UkrainianBankSystem.java
@@ -42,7 +42,7 @@ public class UkrainianBankSystem implements BankSystem {
     public void fund(User user, int amount) {
         if (!checkFunding(user, amount))
             return;
-        else if (amount - amount * user.getBank().getCommission(amount) < user.getBank().getLimitOfFunding())
+        else /*if (amount - amount * user.getBank().getCommission(amount) < user.getBank().getLimitOfFunding())*/
             user.setBalance(user.getBalance() + amount - amount * user.getBank().getCommission(amount));
     }
 
