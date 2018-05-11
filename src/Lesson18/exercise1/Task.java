565c011da026bece97c26a82ab61e14362a42161
Ruslan <ruslanzarichnyi@ukr.net>
2018-05-11 13:21:21 +0300

    try_catch_finally

--- src/Lesson18/exercise1/Task.java
+++ src/Lesson18/exercise1/Task.java
@@ -7,9 +7,18 @@ public class Task {
     public static void main(String[] args) {
         FileStorage fileStorage = new FileStorage();
         String[] fileNames = {"test1", "test2"/*, "test3", "test4", "test5"*/};
+        String[] fileNames2 = {"test1", "test2", "test3", "test4", null};
+        String[] fileNames3 = {"test1", "test2", "test3", "test4", "test5"};
         fileStorage.setFiles(fileNames);
         System.out.println("Start printing name...");
         printer(fileStorage);
+        fileStorage.setFiles(fileNames2);
+        System.out.println("Start printing name...");
+        printer(fileStorage);
+        fileStorage.setFiles(fileNames3);
+        System.out.println("Start printing name...");
+        printer(fileStorage);
+
         System.out.println("Done");
 
     }
@@ -20,7 +29,7 @@ public class Task {
             System.out.println("5th name is " + names[4]);
         }
         catch (Exception e){
-            System.out.println("5h name cdn not be found...");
+            System.out.println("5h name can not be found...");
             //System.err.println(e.getMessage());
         }
         //solution 2
