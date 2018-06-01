1b1cbc4c256a15cab127979b6498541b8d4321e0
Ruslan <ruslanzarichnyi@ukr.net>
2018-06-01 12:11:54 +0300

    Own exceptions

--- src/lesson20/task2_withHomework/Demo.java
+++ src/lesson20/task2_withHomework/Demo.java
@@ -11,6 +11,12 @@ public class Demo {
         Transaction tr123 = new Transaction(1213455, "i;ohlgi", 40, "for drugs",
                 TransactionType.INCOME, new Date());
         TransactionDAO trDAO = new TransactionDAO();
+       /* transactions[0] = new Transaction(1213, "Kiev", 4, "for drugs",
+                TransactionType.OUTCOME, new Date());
+        transactions[1] = new Transaction(1214, "Odessa", 35, "for drugs",
+                TransactionType.OUTCOME, new Date());
+        transactions[2] = new Transaction(1215, "Kiev1", 0, "for drugs",
+                TransactionType.INCOME, new Date());*/
 
         try {
             //System.out.println(Arrays.toString();
