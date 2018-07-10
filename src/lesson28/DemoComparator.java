2c7c7d9213b00c6aac19107911c76920dcceac2e
Ruslan Zarichnyi <ruslanzarichnyi@ukr.net>
2018-07-10 08:39:42 +0300

    Comparator & Comparable

--- src/lesson28/DemoComparator.java
+++ src/lesson28/DemoComparator.java
@@ -10,15 +10,15 @@ public class DemoComparator {
     public static void main(String[] args) throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
 
-        Capability capability1 = new Capability(1001, "ABCD", "Arrr", true, sdf.parse("2007-12-15"));
-        Capability capability2 = new Capability(1005, "Dad", "Zrrrr", false, sdf.parse("2010-12-15"));
-        Capability capability3 = new Capability(900, "Ourd", "Orrrr", true, sdf.parse("2013-12-15"));
-        Capability capability4 = new Capability(900, "jhp=", "Lrrrr", false, sdf.parse("2008-12-15"));
+        Capability capability1 = new Capability(1001, "A", "A", true, sdf.parse("2007-12-15"));
+        Capability capability2 = new Capability(1005, "D", "Z", false, sdf.parse("2010-12-15"));
+        Capability capability3 = new Capability(900, null, "O", true, sdf.parse("2013-12-15"));
+        Capability capability4 = new Capability(900, "B", "L", false, sdf.parse("2008-12-15"));
 
 
         ArrayList<Capability> capabilities = new ArrayList<>();
         capabilities.add(capability1);
-        capabilities.add(capability2);
+        capabilities.add(null);
         capabilities.add(capability3);
         capabilities.add(capability4);
 
